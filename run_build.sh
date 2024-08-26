cecfb5f5fea24875b736c1749c76d2da681a843b
Rafał Cieślak <rafalcieslak256@gmail.com>
2024-08-26 17:23:47 +0200

    Attempt at upgrading to newer toolchain

--- run_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ZMK_VERSION="3.0"
-docker run --rm -it \
-       -v ${PWD}:/zmk \
-       --user ${UID} \
-       --workdir /zmk \
-       zmkfirmware/zmk-build-arm:${ZMK_VERSION} \
-       bash build.sh
